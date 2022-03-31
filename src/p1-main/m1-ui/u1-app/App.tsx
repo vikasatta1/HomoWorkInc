a795683835dbf343cd65b2e1ab3e2ef1bcc1bb12
Vika <Vikasatta1998@gmail.com>
2022-03-31 15:40:53 +0300

    HW4

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import s from './App.module.css'
 import HW3 from "../../../p2-homeworks/h3/HW3";
+import HW4 from "../../../p2-homeworks/h4/HW4";
 
 function App() {
     return (
@@ -8,8 +9,8 @@ function App() {
             <div>react homeworks:</div>
             {/*<HW1/>
             <HW2/>*/}
-            <HW3/>
-            {/*<HW4/>*/}
+            {/*<HW3/>*/}
+            <HW4/>
             {/*<HW5/>*/}
 
         </div>

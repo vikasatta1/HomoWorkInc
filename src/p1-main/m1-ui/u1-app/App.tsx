20b7d15341cd2d55e683caeba443ee990e46f9f1
Vika <Vikasatta1998@gmail.com>
2022-03-31 15:53:13 +0300

    deploy

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import s from './App.module.css'
 import HW3 from "../../../p2-homeworks/h3/HW3";
 import HW4 from "../../../p2-homeworks/h4/HW4";
+import HW5 from "../../../p2-homeworks/h5/HW5";
 
 function App() {
     return (
@@ -10,8 +11,8 @@ function App() {
             {/*<HW1/>
             <HW2/>*/}
             {/*<HW3/>*/}
-            <HW4/>
-            {/*<HW5/>*/}
+            {/*<HW4/>*/}
+            <HW5/>
 
         </div>
     )
